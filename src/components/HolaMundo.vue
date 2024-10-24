79a26f3602e37da4748c1c274fa10bd6e2280811
CPenalba <carolinapenalbacorpa@gmail.com>
2024-10-24 14:12:39 +0200

    first commit

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,19 @@
+<template>
+  <diV>
+    <h1>Mi primera App de Vue, que ilusion!!!</h1>
+  </diV>
+</template>
+
+<script>
+//esto es codigo js (esepecial)
+//cada component que creemos siempre debemos ecportarlo con un name
+export default {
+  name: "HolaMundo",
+};
+</script>
+
+<style>
+h1 {
+  color: fuchsia;
+}
+</style>
